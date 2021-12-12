235778414248ac2b52c8dc7286b5ff176d93d9d2
dedprod <91069952+dedprod@users.noreply.github.com>
2021-12-12 16:22:28 +0300

    Incorrect task

--- Task1.java
+++ Task1.java
@@ -5,7 +5,6 @@ public class Task1 {
     public static void main(String args[]) {
         int n = 0;
         int ent = 0;
-        int ent2 = 0;
         float sum = 0;
 
         try {
@@ -19,18 +18,16 @@ public class Task1 {
             }
             System.out.println("Введите элемент который хотите заменить, нумерация начинается с 0: ");
             ent = console.nextInt();
-            System.out.println("Введите элемент до которого будет вычислятся среднее арифметическое, нумерация начинается с 0: ");
-            ent2 = console.nextInt();
             console.close();
             System.out.print("Исходный массив: ");
             for (float elem : array) {
                 System.out.print(elem + " ");
             }
             System.out.println();
-            for (int g = 0; g <= ent2; g++) {
+            for (int g = 0; g < ent; g++) {
                 sum = sum + array[g];
             }
-            sum = sum / (ent2 + 1);
+            sum = sum / (ent);
             if ((ent <= array.length) & (ent > 0)) {
                 array[ent] = sum;
                 System.out.print("Итоговый массив: ");
